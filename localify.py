0d4001b3ca40f4a0865cd46cde5f89f2f46cc86c
Shubh Patel <shubh.p013@gmail.com>
2023-07-06 00:15:23 -0400

    Update localify.py

--- localify.py
+++ localify.py
@@ -1,49 +1,62 @@
 from flask import Flask, request, render_template, send_file
+import yt_dlp
 import moviepy.editor as mp
-from pytube import YouTube
 import eyed3
+import tempfile
 import os
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  if request.method == 'POST':
-    url = request.form['youtube-link']
-    artist = request.form['artist-name']
-    song = request.form['song-name']
-    cover = request.files['files']
-    cover_data = cover.read()
+    if request.method == 'POST':
+        url = request.form['youtube-link']
+        artist = request.form['artist-name']
+        song = request.form['song-name']
+        cover = request.files['files']
+        cover_data = cover.read()
 
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': '%(title)s.%(ext)s',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
 
-    yt = YouTube(url)
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=True)
+            audio_filename = f"{info_dict['title']}.mp3"
 
-    vidName = yt.title.replace('.', '').replace('"', '').replace('$', '').replace('/', '').replace("'", "").replace("|", "").replace("-", "")
-    yt.streams.get_highest_resolution().download()
+        with tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) as tmp_file:
+            tmp_filename = tmp_file.name
 
-    clip = mp.VideoFileClip(vidName + '.mp4')
-    clip.audio.write_audiofile(song + '.mp3')
+        clip = mp.AudioFileClip(audio_filename)
+        clip.write_audiofile(tmp_filename)
 
-    filename = song + ".mp3"
-    audio_file = eyed3.load(filename)
+        audio_file = eyed3.load(tmp_filename)
+        audio_file.tag.title = song
+        audio_file.tag.album = song
+        audio_file.tag.album_artist = artist
+        audio_file.tag.artist = artist
+        audio_file.tag.images.set(3, cover_data, "image/jpeg")
+        audio_file.tag.save()
 
-    audio_file.tag.title = song
-    audio_file.tag.album = song
-    audio_file.tag.album_artist = artist
-    audio_file.tag.artist = artist
-    audio_file.tag.images.set(3, cover_data, "image/jpeg")
-    audio_file.tag.save()
-    os.remove(clip.filename)
+        try:
+            response = send_file(
+                tmp_filename,
+                as_attachment=True,
+                mimetype='audio/mpeg'
+            )
+            response.headers['Content-Disposition'] = f"attachment; filename={song}.mp3"
+            return response
+        finally:
+            os.remove(audio_filename)
+            os.remove(tmp_filename)
 
-    try:
-        return send_file(
-            song + '.mp3',
-            as_attachment=True,
-            mimetype='audio/mpeg')
-    finally:
-        os.remove(filename)
-
-  return render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
